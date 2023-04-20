3ec07d52f7aa06fac6091ce2859c19a6781471ae
ulises matysiak <ulisesmatysiak2gmail.com>
2023-04-20 08:49:52 -0300

    config

---
+++ negocio/DailyNegocio.cs
@@ -0,0 +1,43 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class DailyNegocio
+    {
+        public List<Daily> listar()
+        {
+            List<Daily> lista = new List<Daily>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select Id, Fecha, Score, Words, Helped from DAILY");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Daily aux = new Daily();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Fecha = (DateTime)datos.Lector["Fecha"];
+                    aux.Score = (int)datos.Lector["Score"];
+                    aux.Words = (string)datos.Lector["Words"];
+                    aux.Helped = (bool)datos.Lector["Helped"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+    }
+}
---
+++ presentacion/frmDaily.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace presentacion
+{
+    partial class frmDaily
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblWords = new System.Windows.Forms.Label();
+            this.lblHelp = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.txtWords = new System.Windows.Forms.TextBox();
+            this.cboHelp = new System.Windows.Forms.ComboBox();
+            this.numScore = new System.Windows.Forms.NumericUpDown();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnDetalle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numScore)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(421, 143);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(292, 281);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(35, 30);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(40, 13);
+            this.lblFecha.TabIndex = 1;
+            this.lblFecha.Text = "Fecha:";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(35, 66);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(38, 13);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score:";
+            // 
+            // lblWords
+            // 
+            this.lblWords.AutoSize = true;
+            this.lblWords.Location = new System.Drawing.Point(35, 107);
+            this.lblWords.Name = "lblWords";
+            this.lblWords.Size = new System.Drawing.Size(41, 13);
+            this.lblWords.TabIndex = 3;
+            this.lblWords.Text = "Words:";
+            // 
+            // lblHelp
+            // 
+            this.lblHelp.AutoSize = true;
+            this.lblHelp.Location = new System.Drawing.Point(35, 403);
+            this.lblHelp.Name = "lblHelp";
+            this.lblHelp.Size = new System.Drawing.Size(35, 13);
+            this.lblHelp.TabIndex = 4;
+            this.lblHelp.Text = "Help?";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(130, 30);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 5;
+            // 
+            // txtWords
+            // 
+            this.txtWords.Location = new System.Drawing.Point(35, 143);
+            this.txtWords.Multiline = true;
+            this.txtWords.Name = "txtWords";
+            this.txtWords.Size = new System.Drawing.Size(295, 232);
+            this.txtWords.TabIndex = 6;
+            // 
+            // cboHelp
+            // 
+            this.cboHelp.FormattingEnabled = true;
+            this.cboHelp.Location = new System.Drawing.Point(130, 403);
+            this.cboHelp.Name = "cboHelp";
+            this.cboHelp.Size = new System.Drawing.Size(100, 21);
+            this.cboHelp.TabIndex = 7;
+            // 
+            // numScore
+            // 
+            this.numScore.Location = new System.Drawing.Point(130, 66);
+            this.numScore.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numScore.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numScore.Name = "numScore";
+            this.numScore.Size = new System.Drawing.Size(37, 20);
+            this.numScore.TabIndex = 8;
+            this.numScore.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(452, 26);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(452, 66);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnDetalle
+            // 
+            this.btnDetalle.Location = new System.Drawing.Point(601, 25);
+            this.btnDetalle.Name = "btnDetalle";
+            this.btnDetalle.Size = new System.Drawing.Size(75, 23);
+            this.btnDetalle.TabIndex = 11;
+            this.btnDetalle.Text = "Ver detalle";
+            this.btnDetalle.UseVisualStyleBackColor = true;
+            // 
+            // frmDaily
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDetalle);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.numScore);
+            this.Controls.Add(this.cboHelp);
+            this.Controls.Add(this.txtWords);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.lblHelp);
+            this.Controls.Add(this.lblWords);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmDaily";
+            this.Text = "Daily";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numScore)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblWords;
+        private System.Windows.Forms.Label lblHelp;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.TextBox txtWords;
+        private System.Windows.Forms.ComboBox cboHelp;
+        private System.Windows.Forms.NumericUpDown numScore;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnDetalle;
+    }
+}
